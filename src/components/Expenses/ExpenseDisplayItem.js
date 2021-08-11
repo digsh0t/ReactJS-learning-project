6089101bab594f11a546f8d9278af65700a39700
Tri Le <wintltr@gmail.com>
2021-08-11 08:32:51 +0700

    Test push

--- src/components/Expenses/ExpenseDisplayItem.js
+++ src/components/Expenses/ExpenseDisplayItem.js
@@ -9,18 +9,28 @@ function DisplayItem(props) {
 
   const yearChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
-  }
+  };
 
-  const filteredExpenses = props.items.filter(expense => {
+  const filteredExpenses = props.items.filter((expense) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
-  
+
   return (
     <div className="expenses">
-      <ExpensesFilter onYearChange={yearChangeHandler}/>
-      {filteredExpenses.map((item) => (
-        <ExpenseItem key={item.id} title={item.title} amount={item.amount} date={item.date} />
-      ))}
+      <ExpensesFilter onYearChange={yearChangeHandler} />
+
+      {filteredExpenses.length === 0 ? (
+        <p>No expenses found</p>
+      ) : (
+        filteredExpenses.map((item) => (
+          <ExpenseItem
+            key={item.id}
+            title={item.title}
+            amount={item.amount}
+            date={item.date}
+          />
+        ))
+      )}
     </div>
   );
 }
--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -6,15 +6,20 @@ import Card from "../UI/Card.js";
 
 function ExpenseItem(props) {
   // function clickHandler() {}
+  const [changedTitle, setTitle] = useState(props.title)
+
+  const changeTitle = () => {
+    setTitle("test");
+  };
 
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date}></ExpenseDate>
       <div classhName="expense-item__description">
-        <h2>{props.title}</h2>
+        <h2>{changedTitle}</h2>
         <div className="expense-item__price">${props.amount}</div>
       </div>
-      <button>Change Title</button>
+      <button onClick={changeTitle}>Change Title</button>
     </Card>
   );
 }
